38ce320f0ef185391fe2233779aeae418bdf99e1
Kat White <kat.white.a@gmail.com>
2021-07-21 13:55:27 -0400

    Try out POST method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,4 +4,25 @@ const getAllReservations = () => {
   );
 };
 
+const postAReservation = (name, date, time, number) => {
+  return fetch("http://localhost:3001/api/v1/reservations", {
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      date: date,
+      time: time,
+      number: number,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+};
+
+// Add postAReservation to the import link
+// replace addReservation spread operator with a the necesscary then statements to add info to the api
+// i _think_ this will automatically re-render when react realizes a change has been made...
+
 export { getAllReservations };
+
+// {name: <String>, date: <String>, time: <String>, number: <Number>}
