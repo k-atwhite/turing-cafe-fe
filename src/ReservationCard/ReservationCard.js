8f7a24f53964a2922b4fd0dbe40805aa742f96dd
Kat White <kat.white.a@gmail.com>
2021-07-21 11:52:18 -0400

    Add basic CSS to render cards

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -4,10 +4,12 @@ import "./ReservationCard.css";
 const Card = ({ name, date, time, number }) => {
   return (
     <div className="reservation-card">
-      <h3>{name}</h3>
-      <p>{date}</p>
-      <p>{time}</p>
-      <p>{number}</p>
+      <div className="reservation-details">
+        <h4>{name}</h4>
+        <p>{date}</p>
+        <p>{time} pm</p>
+        <p>number of guests: {number}</p>
+      </div>
     </div>
   );
 };
