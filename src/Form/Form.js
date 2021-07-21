2ac2a079fb2f247e9fe6e236d744cf09d4ccc93d
Kat White <kat.white.a@gmail.com>
2021-07-21 12:13:10 -0400

    Add Form component and files

---
+++ src/Form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Form.css'
+
+class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            date: '',
+            time: '',
+            number: 0
+        }
+    }
+    
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Form;
